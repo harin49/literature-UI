9865c08f578f50f1bafb6c07811b889c4e7e6d25
Hari Narayanan <harinarayanan4057@gmail.com>
2020-08-19 00:53:18 +0530

    Hari: Add eslint config

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+const OFF = 0;
+const WARN = 1;
+const ERROR = 2;
+
+module.exports = {
+    env: {
+        es6: true,
+        browser: true,
+    },
+    extends: ['airbnb', 'prettier', 'prettier/react', 'plugin:jsx-control-statements/recommended'],
+    plugins: ['react-hooks', 'jsx-control-statements'],
+    rules: {
+        semi: [ERROR, 'always'],
+        "react-hooks/rules-of-hooks": ERROR,
+        "react-hooks/exhaustive-deps": WARN,
+        'no-console': ERROR,
+        "react/jsx-filename-extension": OFF,
+        "react/jsx-wrap-multilines": OFF,
+        "react/jsx-closing-tag-location": OFF,
+        "react/jsx-no-undef": [ERROR, { allowGlobals: true }],
+        "react/jsx-props-no-spreading": OFF,
+        "react/no-multi-comp": ERROR,
+        "react/forbid-prop-types": OFF,
+    },
+    parser: "babel-eslint",
+}   
