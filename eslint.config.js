c4099a2e706effae8fed5b484480e600fc9962ba
Hari Narayanan <harinarayanan4057@gmail.com>
2020-08-21 01:29:22 +0530

    remove eslint loader from webpack-config - not sure if this is the right way to go

--- eslint.config.js
+++ eslint.config.js
@@ -3,25 +3,25 @@ const WARN = 1;
 const ERROR = 2;
 
 module.exports = {
-    env: {
-        es6: true,
-        browser: true,
-    },
-    extends: ['airbnb', 'prettier', 'prettier/react', 'plugin:jsx-control-statements/recommended'],
-    plugins: ['react-hooks', 'jsx-control-statements'],
-    rules: {
-        semi: [ERROR, 'always'],
-        "react-hooks/rules-of-hooks": ERROR,
-        "react-hooks/exhaustive-deps": WARN,
-        'no-console': ERROR,
-        "react/jsx-filename-extension": OFF,
-        "react/jsx-wrap-multilines": OFF,
-        "react/jsx-closing-tag-location": OFF,
-        "react/jsx-no-undef": [ERROR, { allowGlobals: true }],
-        "react/jsx-props-no-spreading": OFF,
-        "react/no-multi-comp": ERROR,
-        "react/forbid-prop-types": OFF,
-        "import/no-unresolved": ERROR
-    },
-    parser: "babel-eslint",
-}   
+  env: {
+    es6: true,
+    browser: true,
+  },
+  extends: ['airbnb', 'prettier', 'prettier/react', 'plugin:jsx-control-statements/recommended'],
+  plugins: ['react-hooks', 'jsx-control-statements'],
+  rules: {
+    semi: [ERROR, 'always'],
+    'react-hooks/rules-of-hooks': ERROR,
+    'react-hooks/exhaustive-deps': WARN,
+    'no-console': ERROR,
+    'react/jsx-filename-extension': OFF,
+    'react/jsx-wrap-multilines': OFF,
+    'react/jsx-closing-tag-location': OFF,
+    'react/jsx-no-undef': [ERROR, { allowGlobals: true }],
+    'react/jsx-props-no-spreading': OFF,
+    'react/no-multi-comp': ERROR,
+    'react/forbid-prop-types': OFF,
+    'import/no-unresolved': ERROR,
+  },
+  parser: 'babel-eslint',
+};
