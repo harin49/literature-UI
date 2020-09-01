b57bbede7f9bb6102b54bb8d8566f861c6d91075
Hari Narayanan <harinarayanan4057@gmail.com>
2020-09-02 00:41:20 +0530

    change yarn start:all command, add background pattern

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 import './App.css';
 
 const App = () => (
-  <div className="max-w-md mx-auto flex p-6 bg-gray-100 mt-10 rounded-lg shadow-xl">
-    <div className="ml-6 pt-1">
-      <h1 className="text-2xl text-blue-700 leading-tight">Configured tailwind at last!</h1>
-      <p className="text-base text-gray-700 leading-normal">Building apps together</p>
+  <div className="min-w-full min-h-full p-6 bg-main-custom">
+    <div className="px-12 py-12 w-1/3 mt-10 bg-gray-100 rounded-lg shadow-xl mx-auto border-l-8 border-purple-300 animate-bounce">
+      <h1 className="text-2xl text-blue-700 leading-tight">Start literature!</h1>
+      <p className="text-base text-gray-700 leading-normal">Check rules</p>
     </div>
   </div>
 );
