f279116df6714a1dda124c5a1b24d4a6cdb01366
Hari Narayanan <harinarayanan4057@gmail.com>
2020-09-20 13:58:33 +0530

    Hari: add game header to mobile portrait screen

--- src/MobilePortraitScreen.js
+++ src/MobilePortraitScreen.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const MobilePortraitScreen = () => {
   return (
-    <div className="m-portrait:flex sm:hidden p-6">
-      <div className="px-4 py-4 mt-48 w-auto bg-c1-gray rounded-lg shadow-xl border-l-8 border-blue-700 animate-bounce">
+    <div className="m-portrait:flex m-portrait:flex-col m-portrait:items-center sm:hidden p-6">
+      <span className="text-6xl text-yellow-600">Literature</span>
+      <div className="px-4 py-4 mt-32 w-auto bg-c1-gray rounded-lg shadow-xl border-l-8 border-blue-700 animate-bounce">
         <span className="text-white text-2xl">Tilt screen to start playing!</span>
       </div>
     </div>
